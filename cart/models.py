c470984deb873a8d787c38fe3ae0be2e59f6ed9d
Shubham Singh <shubham92166@gmail.com>
2023-01-18 00:31:51 +0530

    Added cart functionality

---
+++ cart/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from store.models import Product
+
+# Create your models here.
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length = 100, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
+    
+    def sub_total(self):
+        return self.product.price * self.quantity
