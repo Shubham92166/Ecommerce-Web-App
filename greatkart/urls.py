ec26c1397d4f164f648cf71185aa530d3245a1ef
Shubham Singh <shubham92166@gmail.com>
2023-01-26 00:29:57 +0530

    Added registration and login form

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -18,12 +18,13 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-import store
+import store, account
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
     path('store/', include('store.urls')), 
     path('cart/', include('cart.urls')),
+    path('', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
