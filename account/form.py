68e9f66919fdb233e7dd61346f1ff4d7619f90a4
Shubham Singh <shubham92166@gmail.com>
2023-01-27 00:21:28 +0530

    Implemented resgistration functionality

--- account/form.py
+++ account/form.py
@@ -2,7 +2,40 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget = forms.PasswordInput(attrs={
+        'placeholder' : 'Enter password',
+        'class' : 'form-comtrol'
+        }
+    ))
+
+    confirm_password = forms.CharField(widget = forms.PasswordInput(attrs={
+        'placeholder' : 'Enter confirm password',
+        'class' : 'form-comtrol'
+        }
+    ))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Password do not match!'
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email address'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    
         
--- account/views.py
+++ account/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .form import RegistrationForm
+from .models import Account
+from django.contrib import messages
 
-# Create your views here.
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            username = email.split('@')[0]
+            phone_number = form.cleaned_data['phone_number']
+            user = Account.objects.create_user(first_name = first_name, last_name = last_name, email = email, username = username, password = password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, "Registration successful")
+            return redirect('register')
+
+    else:
+        form = RegistrationForm()
     context = {
         'form' : form,
     }
