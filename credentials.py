60b2341886fee385eb1eac69234135d80f1a5c63
Shubham Singh <shubham92166@gmail.com>
2023-02-04 00:59:59 +0530

    added the credentials file

---
+++ credentials.py
@@ -0,0 +1,14 @@
+credentials = {
+    "email" : "shubham92166@gmail.com",
+    "email_password" : "ccfuqmwoogyrheoo",
+    "secret_key" : "django-insecure-$@a%z!l38-a!^ml+r4(@*u%&y*d29l5ixe)mhwe4=bed@%1y4j",
+    "email_host" : "smtp.gmail.com",
+    "email_port" : 587,
+    "db_engine" : "django.db.backends.mysql",
+    "db_name" : "ecommerce",
+    "db_port" : "3306",
+    "db_user" : "root",
+    "db_password" : "root",
+    "db_host" : "127.0.0.1",
+
+}
