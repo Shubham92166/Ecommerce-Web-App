ca6fc2d3d0356ef9588d8f5875415a51fd4333bd
Shubham Singh <shubham92166@gmail.com>
2023-02-10 03:09:44 +0530

    added dashboard templates and implemented order detail functionality

--- account/urls.py
+++ account/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
 
     path('my-orders/', views.my_orders, name = "my-orders"),
     path('edit-profile/', views.edit_profile, name='edit-profile'),
+    path('change-password/', views.change_password, name = 'change-password'),
+    path('order-detail/<int:order_id>/', views.order_detail, name='order-detail'),
 ]
